63a21d2d6077fbbd20bccccd0eb27b948adfa7cf
Kundan Kumar <kundankr1753@gmail.com>
2023-08-06 23:25:10 +0530

    first commit

---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [state, setState] = useState(() => {
+    const savedValue = localStorage.getItem(key);
+    return savedValue ? JSON.parse(savedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
+
+export default useLocalStorage;
